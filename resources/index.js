089c3ec2ac1c4b15a147a1082637072cf5a33c86
Roxana Popescu <roxanateodorapopescu@yahoo.com>
2020-10-07 11:38:26 +0300

    code review

--- resources/index.js
+++ resources/index.js
@@ -1,4 +1,5 @@
 const seeMenuButton = document.getElementById('MenuButton');
+// ori incep toate cu litera mica, ori toate cu litera mare, nu mixte te rog. recomandarea este ca variabilele sa inceapa cu litera mica, dar eu promovez consistenta
 const MobileMenu = document.getElementById('MobileMenu');
 seeMenuButton.addEventListener('click', function(){
     console.log('click');
