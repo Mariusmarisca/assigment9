40dc1a9d5a6ea71adf2284534f819023d556be4f
Mariusmarisca <70143537+Mariusmarisca@users.noreply.github.com>
2021-01-21 17:51:08 +0200

    edited files

--- resources/index.js
+++ resources/index.js
@@ -1,8 +1,9 @@
 const seeMenuButton = document.getElementById('menuButton');
-// ori incep toate cu litera mica, ori toate cu litera mare, nu mixte te rog. recomandarea este ca variabilele sa inceapa cu litera mica, dar eu promovez consistenta
 const mobileMenu = document.getElementById('mobileMenu');
 const layerWrapper= document.getElementById('layer');
-const containerWrapper=document.getElementById('container')
+const containerWrapper=document.getElementById('container');
+const seeCloseButton = document.getElementById('closeButton');
+
 seeMenuButton.addEventListener('click', function(){
     console.log('click');
   mobileMenu.classList.remove('hidden'); 
@@ -10,7 +11,7 @@ seeMenuButton.addEventListener('click', function(){
   containerWrapper.classList.add('hidden');
 }
 )
-const seeCloseButton = document.getElementById('closeButton');
+
  seeCloseButton.addEventListener('click', function() {
    console.log('click');
    mobileMenu.classList.add('hidden');
